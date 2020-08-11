c57b6151fb590cf39287261d7cd17f89da2dbc3f
ezsky <ezskyyoung@gmail.com>
2020-08-11 20:52:45 +0800

    中文站优化

--- src/config/params.php
+++ src/config/params.php
@@ -11,8 +11,8 @@
 return [
     'cookieValidationKey'   => null,
 
-    'debug.enabled'         => false,
-    'debug.allowedIps'      => ['127.0.0.1'],
+    'debug.enabled'         => true,
+    'debug.allowedIps'      => ['*'],
 
     'db.host'             => 'localhost',
     'db.name'             => 'asset_packagist',
@@ -23,4 +23,24 @@ return [
     'monitoring.flag'       => hiqdev\yii2\monitoring\Module::FLAG_DOMAIN,
     'monitoring.email.to'   => null,
     'monitoring.email.from' => null,
+
+    'copyright.year'            => null,
+    'copyright.years'           => null,
+
+    'organization.url'          => 'https://www.bestyii.com',
+    'organization.name'         => 'BestYii 开发者社区',
+    'organization.options'      => null,
+    'tongji.code'=><<<CODE
+<script>
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?eb81ddd0bcac4d2ea25eb88795130c23";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+</script>
+
+CODE
+
 ];
